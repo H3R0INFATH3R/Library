677403f12932e9e912247501ff0f49a770ac5525
Don <dagem.alemseged1@gmail.com>
2023-01-27 01:16:52 +0300

    Complete add book function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const main = document.querySelector('.books')
 const add = document.querySelector('#add-button')
 const prompt = document.querySelector('.add-prompt')
 const close = document.querySelector('.close-prompt')
-const submit = document.querySelector('.add-submit')
+const submit = document.querySelector('.add-book')
 const bookName = document.querySelector('#book-name')
 const author = document.querySelector('#author')
 const pages = document.querySelector('#pages')
@@ -21,50 +21,44 @@ function addBookToLibrary(book) {
     myLibrary.push(book)
 }
 
-let book1 = new Book('Rich Dad Poor Dad', 'Robert Kiyosaki', 262, true)
-let book2 = new Book('The 7 Habits of Highly Effective People', 'Stephen R. Covey', 358, true)
-let book3 = new Book('Think and Grow Rich', 'Napoleon Hill', 324, false)
-addBookToLibrary(book1)
-addBookToLibrary(book2)
-addBookToLibrary(book3)
-
-for (let element in myLibrary) {
-    const book = myLibrary[element]
-    const displayBook = document.createElement('div')
-    displayBook.classList.add('display-book')
-
-    const displayTitle = document.createElement('p')
-    displayTitle.classList.add('display-title')
-    displayTitle.innerHTML = `${book.title}`
-    displayBook.appendChild(displayTitle)
-
-    const displayAuthor = document.createElement('p')
-    displayAuthor.classList.add('display-author')
-    displayAuthor.innerHTML = `${book.author}`
-    displayBook.appendChild(displayAuthor)
-
-    const displayPages = document.createElement('p')
-    displayPages.classList.add('display-pages')
-    displayPages.innerHTML = `${book.pages} pages`
-    displayBook.appendChild(displayPages)
-
-    const displayStatus = document.createElement('p')
-    displayStatus.classList.add('display-status')
-    if (book.read) {
-        displayStatus.innerHTML = 'Read'
-        displayStatus.style.color = 'green'
-    } else {
-        displayStatus.innerHTML = 'Not Read'
-        displayStatus.style.color ='red'
-    }
-    displayBook.appendChild(displayStatus)
-
-    main.appendChild(displayBook)
+const refreshLibrary = () => {
+    // for (let element in myLibrary) {
+        const book = myLibrary[myLibrary.length - 1]
+        const displayBook = document.createElement('div')
+        displayBook.classList.add('display-book')
+    
+        const displayTitle = document.createElement('p')
+        displayTitle.classList.add('display-title')
+        displayTitle.innerHTML = `${book.title}`
+        displayBook.appendChild(displayTitle)
+    
+        const displayAuthor = document.createElement('p')
+        displayAuthor.classList.add('display-author')
+        displayAuthor.innerHTML = `${book.author}`
+        displayBook.appendChild(displayAuthor)
+    
+        const displayPages = document.createElement('p')
+        displayPages.classList.add('display-pages')
+        displayPages.innerHTML = `${book.pages} pages`
+        displayBook.appendChild(displayPages)
+    
+        const displayStatus = document.createElement('p')
+        displayStatus.classList.add('display-status')
+        if (book.read) {
+            displayStatus.innerHTML = 'Read'
+            displayStatus.style.color = 'green'
+        } else {
+            displayStatus.innerHTML = 'Not Read'
+            displayStatus.style.color ='red'
+        }
+        displayBook.appendChild(displayStatus)
+    
+        main.appendChild(displayBook)
+    // }
 }
 
 add.addEventListener('click', () => {
     prompt.style.visibility = 'visible'
-    // body.style.opacity = '0.2'
     body.style.cssText = `
         font-family: 'Palanquin Dark', sans-serif;
         background-color: gray;
@@ -79,18 +73,23 @@ close.addEventListener('click', () => {
         font-family: 'Palanquin Dark', sans-serif;
         background-color: var(--gray);
         `
-        bookName.value = ''
-        author.value = ''
-        pages.value = ''
+    bookName.value = ''
+    author.value = ''
+    pages.value = ''
 })
 
 // let i = 1
 
 submit.addEventListener('click', () => {
-    // let bookInput = new Book(bookName.value, author.value, pages.value, readStatus.value)
-    // addBookToLibrary(bookInput)
+    let bookInput = new Book(bookName.value, author.value, pages.value, readStatus.value)
+    addBookToLibrary(bookInput)
+    body.style.cssText = `
+        font-family: 'Palanquin Dark', sans-serif;
+        background-color: var(--gray);
+        `
     bookName.value = ''
     author.value = ''
     pages.value = ''
-    console.log('Submited')
+    refreshLibrary()
+    prompt.style.visibility = 'hidden'
 })
