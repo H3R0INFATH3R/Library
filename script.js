5e9f4b14af3ec2f2dd50e70bcfb0c4466ed0593c
Don <dagem.alemseged1@gmail.com>
2023-01-27 14:03:25 +0300

    Fix checkbox issue

--- script.js
+++ script.js
@@ -22,39 +22,37 @@ function addBookToLibrary(book) {
 }
 
 const refreshLibrary = () => {
-    // for (let element in myLibrary) {
-        const book = myLibrary[myLibrary.length - 1]
-        const displayBook = document.createElement('div')
-        displayBook.classList.add('display-book')
-    
-        const displayTitle = document.createElement('p')
-        displayTitle.classList.add('display-title')
-        displayTitle.innerHTML = `${book.title}`
-        displayBook.appendChild(displayTitle)
-    
-        const displayAuthor = document.createElement('p')
-        displayAuthor.classList.add('display-author')
-        displayAuthor.innerHTML = `${book.author}`
-        displayBook.appendChild(displayAuthor)
-    
-        const displayPages = document.createElement('p')
-        displayPages.classList.add('display-pages')
-        displayPages.innerHTML = `${book.pages} pages`
-        displayBook.appendChild(displayPages)
-    
-        const displayStatus = document.createElement('p')
-        displayStatus.classList.add('display-status')
-        if (book.read) {
-            displayStatus.innerHTML = 'Read'
-            displayStatus.style.color = 'green'
-        } else {
-            displayStatus.innerHTML = 'Not Read'
-            displayStatus.style.color ='red'
-        }
-        displayBook.appendChild(displayStatus)
-    
-        main.appendChild(displayBook)
-    // }
+    const book = myLibrary[myLibrary.length - 1]
+    const displayBook = document.createElement('div')
+    displayBook.classList.add('display-book')
+
+    const displayTitle = document.createElement('p')
+    displayTitle.classList.add('display-title')
+    displayTitle.innerHTML = `${book.title}`
+    displayBook.appendChild(displayTitle)
+
+    const displayAuthor = document.createElement('p')
+    displayAuthor.classList.add('display-author')
+    displayAuthor.innerHTML = `${book.author}`
+    displayBook.appendChild(displayAuthor)
+
+    const displayPages = document.createElement('p')
+    displayPages.classList.add('display-pages')
+    displayPages.innerHTML = `${book.pages} pages`
+    displayBook.appendChild(displayPages)
+
+    const displayStatus = document.createElement('p')
+    displayStatus.classList.add('display-status')
+    if (book.read) {
+        displayStatus.innerHTML = 'Read'
+        displayStatus.style.color = 'green'
+    } else {
+        displayStatus.innerHTML = 'Not Read'
+        displayStatus.style.color ='red'
+    }
+    displayBook.appendChild(displayStatus)
+
+    main.appendChild(displayBook)
 }
 
 add.addEventListener('click', () => {
@@ -76,20 +74,26 @@ close.addEventListener('click', () => {
     bookName.value = ''
     author.value = ''
     pages.value = ''
+    readStatus.checked = false
 })
 
 // let i = 1
 
 submit.addEventListener('click', () => {
-    let bookInput = new Book(bookName.value, author.value, pages.value, readStatus.value)
-    addBookToLibrary(bookInput)
-    body.style.cssText = `
-        font-family: 'Palanquin Dark', sans-serif;
-        background-color: var(--gray);
-        `
-    bookName.value = ''
-    author.value = ''
-    pages.value = ''
-    refreshLibrary()
-    prompt.style.visibility = 'hidden'
+    if (bookName.value && author.value && pages.value) {
+        let bookInput = new Book(bookName.value, author.value, pages.value, readStatus.checked)
+        addBookToLibrary(bookInput)
+        body.style.cssText = `
+            font-family: 'Palanquin Dark', sans-serif;
+            background-color: var(--gray);
+            `
+        bookName.value = ''
+        author.value = ''
+        pages.value = ''
+        readStatus.checked = false
+        refreshLibrary()
+        prompt.style.visibility = 'hidden'
+
+        console.log(readStatus.value)
+    }
 })
